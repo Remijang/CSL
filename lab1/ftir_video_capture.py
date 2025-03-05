224867d9244bef94ae034753d66472c30156b3e6
Remijang <wuet0829@gmail.com>
2025-03-05 18:32:02 +0800

    Add slidebars and some process
    Use split function get rgb frames and add slidebars provided by TA, so
    we can adjust the threshold of each color.

--- lab1/ftir_video_capture.py
+++ lab1/ftir_video_capture.py
@@ -12,6 +12,12 @@ cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)  # 0.25 --> manual
 cap.set(cv2.CAP_PROP_EXPOSURE, -1)  # exposure value (depends on the camera)
 
 # other settings you need
+def nothing(x):
+	pass
+cv2.namedWindow('Threshold Sliders')
+cv2.createTrackbar('r','Threshold Sliders',24,255,nothing)
+cv2.createTrackbar('g','Threshold Sliders',71,255,nothing)
+cv2.createTrackbar('b','Threshold Sliders',74,255,nothing)
 
 
 while(True):
@@ -19,7 +25,23 @@ while(True):
 	ret, frame = cap.read()
 
 	# Turn the frame into grayscale
-
+	frame_b, frame_g, frame_r = cv2.split(frame)
+	
+	r_thres = cv2.getTrackbarPos('r', 'Threshold Sliders')
+	g_thres = cv2.getTrackbarPos('g', 'Threshold Sliders')
+	b_thres = cv2.getTrackbarPos('b', 'Threshold Sliders')
+	
+	ret, r = cv2.threshold(frame_r, r_thres, 255, cv2.THRESH_BINARY)
+	ret, g_inv = cv2.threshold(frame_g, g_thres, 255, cv2.THRESH_BINARY_INV)
+	ret, b_inv = cv2.threshold(frame_b, b_thres, 255, cv2.THRESH_BINARY_INV)
+	
+	result = cv2.bitwise_and(r, b_inv, mask = None)
+	result = cv2.bitwise_and(result, g_inv, mask = None)
+	result = cv2.blur(result, (5, 5))
+	ret, result = cv2.threshold(result, 128, 255, cv2.THRESH_BINARY)
+	
+	contours, hierarchy = cv2.findContours(result, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	display = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
 
 	# Perform thresholding
 
@@ -34,7 +56,7 @@ while(True):
 
 
 	# Show the frame
-	cv2.imshow('frame', frame)
+	cv2.imshow('frame', display)
 
 	# Press q to quit
 	if cv2.waitKey(1) & 0xFF == ord('q'):
